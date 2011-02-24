c660cc9cca5ba07bca5dad9419c305e4d3a8c0b8
Ruby Ku <rubyku@Ruby-Kus-MacBook-Pro.local>
2011-02-24 17:57:55 -0600

    changed body to messagebody

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
   default :from => "rubyku85@gmail.com"
 
-  def welcome_email(email, subject, topic, body)
+  def welcome_email(email, subject, topic, messagebody)
       @email = email
       @subject = subject
       @topic = topic 
-      @body = body
+      @messagebody = messagebody
       mail(:to => @email,
            :subject => @subject)
   end
