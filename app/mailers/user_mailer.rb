d737404c82c1cd8f4046c8677dd1293da8ef63ba
Ruby Ku <rubyku@Ruby-Kus-MacBook-Pro.local>
2011-02-24 17:46:07 -0600

    added topic and body into message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ActionMailer::Base
   default :from => "rubyku85@gmail.com"
 
-  def welcome_email(email, subject, body)
+  def welcome_email(email, subject, topic, body)
       @email = email
       @subject = subject
+      @topic = topic 
       @body = body
       mail(:to => @email,
            :subject => @subject)
